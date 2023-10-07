e89eb3497a2180daffddd58ca81ef5627ba377d5
subhajit100 <asubhajit35@gmail.com>
2023-10-07 12:17:15 +0530

    remove ts type errors

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,17 +1,21 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const OnboardingPage = async () => {
   const user = await currentUser();
   if (!user) {
     return null;
   }
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user.id,
     objectId: userInfo?._id,
-    username: userInfo && userInfo.username ? userInfo?.username : user.username,
+    username:
+      userInfo && userInfo.username ? userInfo?.username : user.username,
     name: userInfo && userInfo.name ? userInfo?.name : user.firstName ?? "",
     bio: userInfo && userInfo.bio ? userInfo?.bio : "",
     image: userInfo && userInfo.image ? userInfo?.image : user.imageUrl,
